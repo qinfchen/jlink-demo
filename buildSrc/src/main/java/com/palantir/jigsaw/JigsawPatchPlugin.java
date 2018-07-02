513fd1fe44f9c1c8f42e992ad3bc5c906acfa004
Qinfeng Chen <qchen@palantir.com>
2018-07-02 14:40:02 -0400

    move some logic to jigsaw plugin

--- buildSrc/src/main/java/com/palantir/jigsaw/JigsawPatchPlugin.java
+++ buildSrc/src/main/java/com/palantir/jigsaw/JigsawPatchPlugin.java
@@ -2,33 +2,107 @@ package com.palantir.jigsaw;
 
 import org.gradle.api.Plugin;
 import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.gradle.api.artifacts.Dependency;
+import org.gradle.api.internal.file.IdentityFileResolver;
+import org.gradle.api.java.archives.Manifest;
+import org.gradle.api.java.archives.internal.DefaultManifest;
+import org.gradle.api.specs.Spec;
 import org.gradle.api.tasks.Copy;
+import org.gradle.api.tasks.Exec;
 
 import java.io.File;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class JigsawPatchPlugin implements Plugin<Project> {
 
     @Override
     public void apply(Project project) {
-        project.getExtensions().create("needsPatching", NeedsPatchingExtension.class);
+        NeedsPatchingExtension extension = project.getExtensions().create("needsPatching", NeedsPatchingExtension.class);
 
         project.getConfigurations().create("needsPatching");
-
         Copy unpack = project.getTasks().create("unpack", Copy.class);
+//        Exec jdeps = project.getTasks().create("jdeps", Exec.class);
+//        Copy merge = project.getTasks().create("unpack", Copy.class);
+        Exec compileModuleInfo = project.getTasks().create("compileModuleInfo", Exec.class);
+//        Copy copyJarForPatching = project.getTasks().create("copyJarForPatching", Copy.class);
+//        Exec patchJar  = project.getTasks().create("patchJar", Exec.class);
 
         project.afterEvaluate(p -> {
-           unpack.from(project.zipTree(JigsawPatchPlugin.getJarPath(project)));
-           unpack.into(new File("build/unpack/" + JigsawPatchPlugin.getJarName(project)));
+            extension.getModules().stream().forEach(entry -> {
+                File jarFile = JigsawPatchPlugin.getJarPath(
+                        project, "needsPatching", Objects.toString(entry));
+                String jarName = jarFile.getName().replace(".jar", "");
+                unpack.from(project.zipTree(jarFile));
+                unpack.into(new File("build/unpack/" + jarName));
+
+                File input = new File(String.format("%s/unpack/%s/module-info.java",
+                        project.getBuildDir(), jarName));
+                File manifestFile = new File(String.format("%s/unpack/%s/META-INF/MANIFEST.MF",
+                        project.getBuildDir(), jarName));
+                File outputDir = new File(String.format("%s/compileModuleInfo/%s",
+                        project.getBuildDir(), jarName));
+
+
+                System.out.println(manifestFile.toString());
+
+                Task merge = project.getTasks().getByName("merge");
+                compileModuleInfo.dependsOn(merge);
+                Manifest manifest = new DefaultManifest(manifestFile, new IdentityFileResolver());
+                Object module = manifest.getAttributes().get("Automatic-Module-Name");
+                Logger.getGlobal().log(Level.INFO, module.toString());
+                System.out.println(module);
+                List<String> commands = new ArrayList<>();
+                commands.add("javac");
+                commands.add("-d");
+                commands.add(outputDir.toString());
+                commands.add("--module-path");
+                commands.add("../jackson-core/build/patchJar/jackson-core-2.9.5.jar:../jackson-annotations/build/patchJar/jackson-annotations-2.9.5.jar");
+                commands.add("--add-modules");
+                commands.add((String) module);
+                commands.add("--patch-module");
+                commands.add(String.format("%s=%s/unpack/%s", module, project.getBuildDir(),jarName));
+                commands.add(input.toString());
+
+                compileModuleInfo.setCommandLine(commands);
+                compileModuleInfo.getOutputs().dir(outputDir);
+            });
         });
     }
 
-    public static File getJarPath(Project project) {
-        Set<File> files = project.getConfigurations().getByName("needsPatching").getResolvedConfiguration().getFiles();
-        return files.stream().findFirst().get();
+    public static File getJarPath(Project project, String config, String maven) {
+        DependencySpec dependencySpec = new DependencySpec(Objects.toString(maven));
+        return project.getConfigurations().maybeCreate(config).getResolvedConfiguration()
+                .getFiles(dependencySpec).stream().findFirst().get();
     }
 
-    public static String getJarName(Project project) {
-        return getJarPath(project).getName().replace(".jar", "");
+    public static class DependencySpec implements Spec<Dependency> {
+        private final String group;
+        private final String name;
+
+        public DependencySpec(String maven) {
+            String[] mavenCoordinates = maven.split(":");
+            if (mavenCoordinates.length == 2) {
+                group = mavenCoordinates[0];
+                name = mavenCoordinates[1];
+            } else {
+                group = "";
+                name = "";
+            }
+        }
+
+        @Override
+        public boolean isSatisfiedBy(Dependency dependency) {
+            if (dependency.getGroup().equals(group)
+                    && dependency.getName().equals(name)) {
+                return true;
+            }
+            return false;
+        }
     }
+
 }
--- buildSrc/src/main/java/com/palantir/jigsaw/NeedsPatchingExtension.java
+++ buildSrc/src/main/java/com/palantir/jigsaw/NeedsPatchingExtension.java
@@ -1,7 +1,19 @@
 package com.palantir.jigsaw;
 
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
 public class NeedsPatchingExtension {
 
-    String module;
+    private final Set<String> modules = new HashSet<String>();
 
+    public final void implementation(String module) {
+        modules.add(module);
+    }
+
+    public final Set<String> getModules() {
+        return Collections.unmodifiableSet(modules);
+    }
 }
